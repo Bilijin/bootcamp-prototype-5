c94731b99c77e85f254769b706f07f738468fd27
Alabi Bolaji <alabibolaji78@gmail.com>
2021-09-24 11:00:07 +0100

    completed challenge

---
+++ Assets/Scripts/DifficultyButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyButton : MonoBehaviour
+{
+    private Button button;
+    public int difficulty;
+    private GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(setDifficulty);
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void setDifficulty()
+    {
+        gameManager.StartGame(difficulty);
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+
+    private Rigidbody targetRB;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float torqueRange = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -4;
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRB = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+        targetRB.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRB.AddTorque(new Vector3(RandomTorque(), RandomTorque(), RandomTorque()));
+
+        transform.position = RandomSpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            Destroy(gameObject);
+            gameManager.UpdateScore(pointValue);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+        
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-torqueRange, torqueRange);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+}
